e3f218bd55ba76d01911ce8ef9f467b543a912e2
ThomasEmilsson <gusemith@student.gu.se>
2019-02-06 01:47:30 +0100

    working version

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-// server.js
-// where your node app starts
 
-// init project
 var express = require('express');
+var ip = require('ip');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -18,13 +16,19 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/whoami", function(req, res) {
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  var language = req.headers["accept-language"];
+  var software = req.headers["user-agent"];
+
+  res.json(
+    {
+      ip: ip.address(),
+      language: language,
+      software: software
+    }
+  );
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
